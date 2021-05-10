0075672c52788e9a6122f8b40e3d4003854ca196
edriankira <65101229+edriankira@users.noreply.github.com>
2021-05-11 02:49:58 +0800

    bugged home fixed login

--- connection/config.php
+++ connection/config.php
@@ -4,13 +4,13 @@ server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'dbadmin');
+define('DB_NAME', 'sisappdev');
  
 /* Attempt to connect to MySQL database */
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
 // Check connection
-if($link === false){
+if($db === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 ?>
--- login.php
+++ login.php
@@ -1,61 +1,63 @@
+<?php
+
+require_once "connection/config.php";
+
+$error ="";
+
+if(isset($_POST["login"])){
+    $loginuser = mysqli_real_escape_string($db,$_POST['log_username']);
+    $loginpass = mysqli_real_escape_string($db,$_POST['log_password']);
+
+    $sql = "SELECT * FROM adm_AdminUser WHERE adm_username = '$loginuser'";
+    $result = mysqli_query($db,$sql);
+    $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+    $count = mysqli_num_rows($result);
+      
+    if($count == 1) {
+         //session_register("myusername"); depricated/removed from passed ver
+
+         $dbusername = $row['adm_username'];
+         $dbpassword = $row['adm_password'];
+
+         //
+         if ($dbusername == $loginuser && $loginpass == password_verify($loginpass, $dbpassword)) {
+         $_SESSION['login_user'] = $myusername;
+        
+         header("location: homepage.php");
+        
+      }
+      else{
+          $error = "Your Login Name or Password is invalid";
+      }
+   }
+   mysqli_close($db);
+}
+
+?>
+
 <!DOCTYPE HTML>
 <html>
 	<head>
 		<title>Generic - Editorial by HTML5 UP</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-		<link rel="stylesheet" href="assets/css/main.css" />
-		<style type="text/css">
-			.loginDiv{
-				width: 400px; 
-                padding: 20px; 
-                margin: auto;
-                border: 3px 
-                solid black;
-                padding: 30px; 
-                color: white;
-                background-color: black;
-                opacity: 0.8;
-                border-radius: 25px;
-			}
-		</style>
+        <link rel="stylesheet" href="CSS/GeneralCSS.css" />
+        <link rel="stylesheet" href="CSS/login.css" />
 	</head>
-	<body class="is-preload">
-	<!-- Wrapper -->
-		<div id="wrapper">
-
-			<!-- Main -->
-			<div id="main">
-			<div class="inner">
-
-				<!-- Content -->
-			<section>
-				<header class="main">
-										
-				</header>
-				<div class="loginDiv">
-				<h2>Login here</h2>
-				<div class="col-6 col-12-xsmall">
-				<input type="text" name="demo-name" id="demo-name" value="" placeholder="Name" />
-				</div><br/>
-				<div class="col-6 col-12-xsmall">
-				<input type="email" name="demo-email" id="demo-email" value="" placeholder="Email" />
-				</div>		
-				<form method="post" action="#">
-				<!-- Break -->
-				<div class="col-12">
-					<br/>
-				<ul class="actions"><center>
-				</center>
-				<li><input type="submit" value="Login" class="primary" /></li>
-				<li><input type="reset" value="Register" /></li>
-				</ul>
-				</div>
-				</div>
-				</form>
-				</div>								
-			</section>
-				</div>
-				</div>
+	<body>
+        <form method="post">    
+            <div class="loginDiv">
+                <h2>Admin Login</h2>
+                <p>Username<br>
+                    <input type="text" name="log_username" placeholder="Enter Username" />
+                </p>
+                <p>Password<br>
+                    <input type="password" name="log_password" placeholder="Enter Password" /> <br>
+                </p>
+                <input id="loginbtn" name="login" type="submit" value="Login"/>
+                <p><?php echo "$error"?></p>
+            </div>
+        </form>
 	</body>
 </html>
+
