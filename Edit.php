7348a8420d4977aa3c58d3181f38e54125a61e7c
edriankira <65101229+edriankira@users.noreply.github.com>
2021-05-14 00:15:08 +0800

    stable edit?

--- Edit.php
+++ Edit.php
@@ -1,122 +1,10 @@
 <?php
 require_once "connection/config.php";
- 
-
-$pfname = $pmname = $plname = $pbday = $gender = $pemail= $pmobile= $paddress= $pid= $pusername = "";
+$pfname = $pmname = $plname = $pbday = $pgender = $pemail= $pmobile = $paddress= $pid= $pusername = "";
 $pfname_err = $pmname_err = $plname_err = $pusername =$pbday_err = $gender_err = $pemail_err= $pmobile_err= $address_err= $pid_err = "";
 
-if(isset($_POST["id"]) && !empty($_POST["id"])){
 
-    $id = $_POST["id"];
-   
-    //first name
-    $input_fname = trim($_POST["fname"]); 
-    if(empty($input_fname)){
-        $pfname_err = "Please enter a first name."; 
-    } elseif(!filter_var($input_fname, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
-        $pfname_err = "Please enter a valid first name.";
-    } else{
-        $pfname = $input_fname;
-    }
-
-    //middle name
-    if(!filter_var($input_mname, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
-        $pmname_err = "Please enter a valid middle name.";
-    } else{
-        $pmname = $input_mname;
-    }
-
-    //last name
-    $input_lname = trim($_POST["lname"]);
-    if(empty($input_lname)){
-        $name_err = "Please enter a last name."; 
-    } elseif(!filter_var($input_lname, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
-        $name_err = "Please enter a valid last name.";
-    } else{
-        $plname = $input_lname;
-    }
-    
-    //email
-    $input_email = trim($_POST["email"]);
-    if (filter_var($input_email, FILTER_VALIDATE_EMAIL)) {
-        $pemail = $input_email;
-    } else {
-    $pemail_err = "$email is not a valid email address";
-    }
-
-    $input_bday = trim($_POST["bday"]);
-    if(empty($input_lname)){
-        $pbday_err = "Please enter a birthday."; 
-    }
-    else{
-        $pbday = date('Y-m-d', strtotime($_POST['dateFrom']));
-    }
-
-    $input_mobile = trim($_POST["mobile"]);
-    if(empty($input_mobile)){
-        $pmobile_err = "Please enter a mobile number.";
-    }else if(!is_numeric($input_mobile)){
-        $pmobile = $input_mobile;
-    }
-    else{
-        $pmobile_err = "Please enter a valid number";
-    }
-
-    $input_address = trim($_POST["address"]);
-    if(empty($input_address)){
-        $paddress_err = "Please enter an address."; 
-    }else{
-        $paddress = $input_address;
-    }
-
-    $input_username = trim($_POST["username"]);
-    if(empty($input_username)){
-        $username_err = "Please enter a username."; 
-    } else{
-        $pusername = $input_username;
-    }
-
-    if(empty($pfname_err) && empty($pmname_err) &&empty($plname_err) && 
-    empty($pbday_err) && empty($pemail_err) && empty($address_err) &&
-    empty($pusername_err) &&empty($pgender_err)){
-        $sql = "UPDATE adm_adminUser SET adm_username=?, adm_bday=?,  adm_fname = ?,adm_mname = ?, 
-        adm_lname = ?,adm_email = ?, adm_mobile = ?, adm_address = ?, adm_gender =?
-         WHERE adm_adminUserNum = ?";
-         
-        if($stmt = mysqli_prepare($db, $sql)){
-            mysqli_stmt_bind_param($stmt, "sssssssssi",
-            $param_username, $paam_bday,$param_fname,$param_mname,$param_lname,
-            $param_email,$param_mobile,$param_address,$param_gender, $param_id);
-            
-              
-            $param_username = $pusername;
-            $param_bday = $pbday;
-            $param_fname = $pfname;
-            $param_mname = $pmname ;
-            $param_lname = $plname ;
-            $param_email = $pemail;
-            $param_mobile = $pmobile;
-            $param_address = $paddress;
-            $param_gender = $gender ;
-            $param_id = $id;
-            
-            
-            if(mysqli_stmt_execute($stmt)){
-                header("location: read.php?id=$id");
-                exit();
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
-            }
-        }
-         
-        mysqli_stmt_close($stmt);
-    }
-    
-    mysqli_close($db);
-    
-}else{
     if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
-        require_once "connection/config.php";
         $sql = "SELECT * FROM adm_adminuser WHERE adm_adminId = ?";
         
         if($stmt = mysqli_prepare($db, $sql)){
@@ -139,8 +27,9 @@ if(isset($_POST["id"]) && !empty($_POST["id"])){
                     $pemail = $row['adm_email'];
                     $paddress = $row['adm_address'];
                     $pusername = $row['adm_username'];
-
-
+                    
+                    $newdate = date('d-m-Y', strtotime($row['adm_bday']));
+                    
                 } else{
                     header("location: error.php");
                     exit();
@@ -151,11 +40,126 @@ if(isset($_POST["id"]) && !empty($_POST["id"])){
             }
         }
      
-        mysqli_stmt_close($stmt);
-        
-        mysqli_close($db);
+        mysqli_stmt_close($stmt);        
     }
-}   
+    
+    if(isset($_POST['goEdit'])){
+
+        $id = $_GET["id"];
+        //first name
+        $input_fname = trim($_POST["fname"]); 
+        if(empty($input_fname)){
+            $pfname_err = "Please enter a first name."; 
+        } elseif(!filter_var($input_fname, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
+            $pfname_err = "Please enter a valid first name.";
+        } else{
+            $pfname = $input_fname;
+            
+        }
+        
+        //middle name
+        $input_mname = trim($_POST["mname"]);
+        if(empty($input_mname)){
+            $pmname = $input_mname;
+        }
+        else if(!filter_var($input_mname, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
+            $pmname_err = "Please enter a valid middle name.";
+        } else{
+            $pmname = $input_mname;
+        }
+    
+        //last name
+        $input_lname = trim($_POST["lname"]);
+        if(empty($input_lname)){
+            $plname_err = "Please enter a last name."; 
+        } elseif(!filter_var($input_lname, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
+            $plname_err = "Please enter a valid last name.";
+        } else{
+            $plname = $input_lname;
+        }
+
+        //gender
+        $input_gender= trim($_POST["gender"]);
+        $pgender = $input_gender;
+        
+        //email
+        $input_email = trim($_POST["email"]);
+        if (filter_var($input_email, FILTER_VALIDATE_EMAIL)) {
+            $pemail = $input_email;
+        } else {
+            $pemail_err = $input_email . " is not a valid email address";
+        }
+    
+        $input_bday = trim($_POST["bday"]);
+        if(empty($input_bday)){
+            $pbday_err = "Please enter a birthday."; 
+        }
+        else{
+            $pbday = date('Y-m-d', strtotime($_POST['bday']));
+            echo "$input_bday";
+        }
+    
+        $input_mobile = trim($_POST["mobile"]);
+        if(empty($input_mobile)){
+            $pmobile_err = "Please enter a mobile number.";
+        }else if(is_numeric($input_mobile)){
+            $pmobile = $input_mobile;
+        }
+        else{
+            $pmobile_err = "Please enter a valid number";
+        }
+    
+        $input_address = trim($_POST["address"]);
+        if(empty($input_address)){
+            $paddress_err = "Please enter an address."; 
+        }else{
+            $paddress = $input_address;
+        }
+    
+        $input_username = trim($_POST["username"]);
+        if(empty($input_username)){
+            $username_err = "Please enter a username."; 
+        } else{
+            $pusername = $input_username;
+        }
+    
+        if(empty($pfname_err) && empty($pmname_err) &&empty($plname_err) && 
+        empty($pbday_err) && empty($pemail_err) && empty($address_err) &&
+        empty($pusername_err) &&empty($pgender_err)){
+            $sql = "UPDATE adm_adminUser SET adm_username=?, adm_bday=?,  adm_fname = ?,adm_mname = ?, 
+            adm_lname = ?,adm_email = ?, adm_mobile = ?, adm_address = ?, adm_gender =?
+             WHERE adm_adminId = ?";
+            
+            if($stmt = mysqli_prepare($db, $sql)){
+                mysqli_stmt_bind_param($stmt, "sssssssssi",
+                $param_username, $param_bday,$param_fname,$param_mname,$param_lname,
+                $param_email,$param_mobile,$param_address,$param_gender, $param_id);
+                
+                  
+                $param_username = $pusername;
+                $param_bday = $pbday;
+                $param_fname = $pfname;
+                $param_mname = $pmname ;
+                $param_lname = $plname ;
+                $param_email = $pemail;
+                $param_mobile = $pmobile;
+                $param_address = $paddress;
+                $param_gender = $pgender;
+                $param_id = $id;
+                
+                
+                if(mysqli_stmt_execute($stmt)){
+                    // header("location: read.php?id=$id");
+                    // exit();
+                } else{
+                    echo "Oops! Something went wrong. Please try again later.";
+                }
+            }else echo "error 501";
+             
+            mysqli_stmt_close($stmt);
+            mysqli_close($db);
+        }else echo "error 502";
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -185,7 +189,7 @@ input[type="text"]:focus,input[type="email"]:focus{
     border: 2px solid #f56a6a;
 }
 input[type="date"]{
-    width: 110px;
+    width: 80%;
 }
 td, th{ 
     text-align: center;
@@ -223,7 +227,7 @@ td, th{
             </tr>
             <tr>
                 <td><?php 
-                echo '<input type ="date" name="bday" value='. $row["adm_bday"].' > <br><b>Birthdate<b></td>';
+                echo '<input type ="date" name="bday" value='. $row["adm_bday"].'> <br><b>Birthdate<b></td>';
                 if($row['adm_gender'] == "male" || $row['adm_gender'] == "Male"){
                     echo '<td> <select name="gender">
                                     <option selected value="male">male</option>
@@ -245,7 +249,7 @@ td, th{
             </tr>
             <tr>
                 <td colspan = 2 id="emailt"><?php echo '<input name ="email" id="emailText" type ="email" value='. $row["adm_email"].' ><br><b>Email</b></td>
-                <td> <input type ="text" name="mobile" id="mobileText" value='. $row["adm_mobile"].' ><br><b>Mobile<b></td>
+                <td><input name="mobile" id="mobileText" type ="text" value='. $row["adm_mobile"].' > <br><b>Mobile<b></td>
             </tr>'; ?>
             <tr>
                 <td colspan=3 id="firstline"></td>
@@ -256,8 +260,9 @@ td, th{
                 ?><br><b>Address</b></td>
             </tr>
         </table>
-        <input type="submit" id="back" value="Save"/><br> 
+        <input type="submit" name="goEdit" id="save" value="Save"/><br> 
         </form>
+        
         <input type="button" class="backread" value="Back" onClick="location.href='AdminRegistration.php'">
 </body>
 </html>
